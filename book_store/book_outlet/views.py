7762d978d7d31bb0220f301342cc6a7612111119
Brian Nyingambe <briannmbe@gmail.com>
2022-07-08 22:58:25 +0300

    add the slug fields and 404 page

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from audioop import avg
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
@@ -6,7 +9,28 @@ from .models import Book
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('title')
+    number_of_books = books.count()
+    avg_rating = books.aggregate(Avg('ratings'))
+
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_num_of_books": number_of_books,
+        "ave_ratings": avg_rating
+    })
+
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404() # same as
+
+    book = get_object_or_404(Book, slug=slug)
+
+    return render(request, 'book_outlet/book_detail.html', {
+        "title": book.title,
+        "author": book.author,
+        "ratings": book.ratings,
+        "is_bestselling": book.is_bestselling
     })
