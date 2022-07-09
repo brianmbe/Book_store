a0dd0ee2c96135ae97b23eaea6b196517758d80e
Brian Nyingambe <briannmbe@gmail.com>
2022-07-09 20:35:41 +0300

    add admin

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("title", "ratings",)
+    list_display = ("title", "author", "ratings",)
+
+
+admin.site.register(Book, BookAdmin)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -7,21 +7,41 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    """
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
+    """
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     ratings = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
 
-    # Overwriting the inbuilt save in mysql
+    # Overwriting the inbuilt save() in mysql
+    # Giving the user the right to input his/her slug
+    """
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+    """
 
     def __str__(self):
-        return f"{self.title} ({self.ratings} and is {self.is_bestselling})"
+        return f"{self.title} (Ratings: {self.ratings} and Bestseller: {self.is_bestselling})"
