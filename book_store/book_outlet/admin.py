43bbbbf0b96fcf2e28daf99caf9172320e4cbbc3
Brian Nyingambe <briannmbe@gmail.com>
2022-07-11 22:53:02 +0300

    improve on the reliability og the admin.py

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_filter = ("title", "ratings",)
+    list_filter = ("author", "ratings",)
     list_display = ("title", "author", "ratings",)
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name',)
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_filter = ("name",)
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Address)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,3 +1,5 @@
+import code
+from tabnanny import verbose
 from turtle import title
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -7,18 +9,47 @@ from django.utils.text import slugify
 # Create your models here.
 
 
-class Author(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=3)
 
-    """
     class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
+        verbose_name_plural = "Countries"
 
     def __str__(self):
-        return self.name
+        return f'{self.name}, ({self.code})'
 
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=8)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Point"
+
+    """
+    def get_absolute_url(self):
+        return reverse("Adress_detail", kwargs={"pk": self.pk})
+    """
+
+
+class Author(models.Model):
+    # Fields
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    adress = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+    """
     def get_absolute_url(self):
         return reverse("_detail", kwargs={"pk": self.pk})
     """
@@ -28,12 +59,17 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     ratings = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
+
+    def __str__(self):
+        return f"{self.title} (Ratings: {self.ratings} and Bestseller: {self.is_bestselling})"
 
     # Overwriting the inbuilt save() in mysql
     # Giving the user the right to input his/her slug
@@ -42,6 +78,3 @@ class Book(models.Model):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
     """
-
-    def __str__(self):
-        return f"{self.title} (Ratings: {self.ratings} and Bestseller: {self.is_bestselling})"
